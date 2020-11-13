53e2b94589a3cb8adc8d3b0224de4e4ddb84e954
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-11-13 15:22:14 +1100

    Refactored test

--- test/IncomingNoteSpec.hs
+++ test/IncomingNoteSpec.hs
@@ -8,6 +8,7 @@ import Data.Aeson
 import Model (IncomingNote(..), NoteIdAndVersion(..))
 import qualified Data.Text    as T
 
+
 unit_your_decodes_IncomingNote :: Assertion
 unit_your_decodes_IncomingNote =
   let incomingJSON =
@@ -20,6 +21,7 @@ unit_your_decodes_IncomingNote =
     case result of
       (Error x)       -> assertFailure $ "Decoding failed with: " <> x
       (Success value) -> value @?= (IncomingNote "blah" Nothing)
+
 
 unit_your_decodes_IncomingNote_with_version_id :: Assertion
 unit_your_decodes_IncomingNote_with_version_id =
@@ -35,3 +37,32 @@ unit_your_decodes_IncomingNote_with_version_id =
     case result of
       (Error x)       -> assertFailure $ "Decoding failed with: " <> x
       (Success value) -> value @?= (IncomingNote "blah" (Just $ NoteIdAndVersion 1000 1))
+
+
+unit_your_decodes_IncomingNote_without_noteId :: Assertion
+unit_your_decodes_IncomingNote_without_noteId =
+  assert_should_not_have_noteId_or_noteVersion $
+    object
+      [
+        "noteText"     .= ("blah" :: T.Text)
+      , "noteVersion" .=  (1 :: Int)
+      ]
+
+
+unit_your_decodes_IncomingNote_without_noteVersion :: Assertion
+unit_your_decodes_IncomingNote_without_noteVersion =
+  assert_should_not_have_noteId_or_noteVersion $
+    object
+      [
+        "noteText"     .= ("blah" :: T.Text)
+      , "noteId"       .= (1000  :: Int)
+      ]
+
+
+assert_should_not_have_noteId_or_noteVersion :: Value -> Assertion
+assert_should_not_have_noteId_or_noteVersion jsonValue =
+  let result = (fromJSON jsonValue :: Result IncomingNote)
+  in
+    case result of
+      (Error x)       -> x @?= "You need to supply both 'noteId' and 'noteVersion' or omit both"
+      (Success value) -> assertFailure $ "Expected decoding failure but got: " <> (show value)
